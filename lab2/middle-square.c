0d214d9dca55ae0e916df20b985bce0f419968bd
Justin Bull <me@justinbull.ca>
2013-03-30 16:06:36 -0400

    STS and middle-square utility

---
+++ lab2/middle-square.c
@@ -0,0 +1,79 @@
+// Justin Bull 500355958
+// Jonathan Kwan 500342079
+
+#define DEBUG 1
+#define DEBUG_LEVEL 2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <limits.h>
+#include <time.h>
+#include "middle-square.h"
+
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  char *progname = argv[0];
+  if (argc < 2)
+  {
+    print_help(progname);
+    exit(EXIT_FAILURE);
+  }
+  int maxSamples = atoi(argv[1]);
+
+  // check for seed
+  int initSeed = 0;
+  if(argc >= 3)
+  {
+    initSeed = atoi(argv[2]);
+    debug_print(1, "Initial seed of %i provided!\n", initSeed);
+  }
+  else
+  {
+    initSeed = 1000 + (time(NULL) % 9000);
+    debug_print(1, "Using time(NULL) to obtain 4-digit seed\n", "");
+  }
+
+  debug_print(1, "Going through %i samples of middle square\n", maxSamples);
+  debug_print(1, "Using initial seed value: %i\n", initSeed);
+
+  int x = initSeed;
+  int newSeed;
+  int *storageArray;
+  storageArray = (int *) malloc(sizeof(int) * maxSamples);
+  for( i = 0; i < maxSamples; i++)
+  {
+    // take previous seed or initial number
+    newSeed = x * x;
+    debug_print(2, "%i * %i = %i\n", x, x, newSeed);
+
+    // select the middle four digits as output and next seed
+    int oldSeed = newSeed;
+    newSeed = (newSeed / 100) % 10000;
+    debug_print(2, "(%i / 100) %% 10000 = %i\n", oldSeed, newSeed);
+
+    // set the new seed as previous seed for next iteration
+    x = newSeed;
+
+    // store as output as well
+    storageArray[i] = x;
+
+    debug_print(1, "byte %i of %i: 0x%.2X (%i)\n", i+1, maxSamples, x, x);
+  }
+  for( i = 0; i < maxSamples; i++)
+  {
+    printf(BYTETOBINARYPATTERN, BYTETOBINARY(storageArray[i]));
+  }
+
+  free(storageArray); storageArray = NULL;
+  exit(EXIT_SUCCESS);
+}
+
+void print_help(char *prgnme) {
+  printf("Usage: %s bytelength [seed]\n\n", prgnme);
+  printf(" ** Omitting [seed] will cause the program to use time(NULL) ** \n");
+  printf(" ** An ASCII binary stream will print to stdout for use with STS ** \n\n");
+}
---
+++ lab2/middle-square.h
@@ -0,0 +1,33 @@
+// Justin Bull 500355958
+// Jonathan Kwan 500342079
+
+#ifndef MIDSQR_H
+#define MIDSQR_H
+
+void print_help(char *prgnme);
+
+/* debug and utility functions */
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL 1
+#endif
+
+#define debug_print(level, fmt, ...) \
+        do { if (DEBUG && level <= DEBUG_LEVEL) fprintf(stderr, "%s:%d:%s(): [DEBUG %i] " fmt, __FILE__, \
+                                __LINE__, __func__, level, __VA_ARGS__); fflush(stderr); } while (0)
+
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
+#define BYTETOBINARYPATTERNSPACE "%d %d %d %d %d %d %d %d"
+#define BYTETOBINARY(byte)  \
+  (byte & 0x80 ? 1 : 0), \
+  (byte & 0x40 ? 1 : 0), \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0)
+
+#endif /* MIDSQR_H */
