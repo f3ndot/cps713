6b3aaeadc5a3f7cbfb7c0a5da8eaf996a3f879ec
Justin Bull <me@justinbull.ca>
2013-03-31 14:04:55 -0400

    better entropy

--- lab2/c-rand-keystroke.c
+++ lab2/c-rand-keystroke.c
@@ -29,7 +29,7 @@ unsigned int lab2_srand(unsigned int seed)
 int main(int argc, char *argv[])
 {
   int i = 0;
-  int initSeed = 0;
+  unsigned long initSeed = 0;
   char *progname = argv[0];
   if (argc < 2)
   {
@@ -48,21 +48,20 @@ int main(int argc, char *argv[])
     numOfKeystrokes = atoi(argv[2]);
   }
 
+  debug_print(2, "UINT_MAX is %lu\n", (unsigned long) UINT_MAX);
   for (i = 0; i < numOfKeystrokes; ++i)
   {
     int tmp = getchar();
     gettimeofday(&tv, NULL);
 
     debug_print(1, "Got char %i at usec %i\n", tmp, tv.tv_usec);
-    initSeed = initSeed ^ tv.tv_usec ^ tmp;
-    debug_print(1, "Seed currently %i at round %i/10\n", initSeed, i+1);
+    initSeed = ((initSeed ^ tv.tv_usec) * tmp) % UINT_MAX;
+    debug_print(1, "Seed currently %lu at round %i/10\n", initSeed, i+1);
   }
 
-  exit(EXIT_SUCCESS);
-
-
   debug_print(1, "Going through %i samples of C-based rand()\n", maxSamples);
-  debug_print(1, "Using initial seed value: %i\n", initSeed);
+  debug_print(1, "Using initial seed value: %lu\n", initSeed);
+  lab2_srand((unsigned int) initSeed);
 
   int *storageArray;
   storageArray = (int *) malloc(sizeof(int) * maxSamples);
